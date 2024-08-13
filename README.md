e348742e13e6f83d1d4dc9c57dc3472f3580ce1e
Florian Duport <f.duport@programisto.fr>
2024-08-13 11:42:37 +0200

    add readme and security

---
+++ README.md
@@ -0,0 +1,32 @@
+# EDRM Webhook mangement module
+
+## Table of Contents
+- [Description](#description)
+- [Features](#features)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Environment Variables](#environment-variables)
+- [Routes](#routes)
+
+
+## Description
+This module is a webhook management. It provides all the implementation to register and trigger webhooks. 
+
+## Features
+- Register a new webhook
+- Update / delete a webhook
+- Trigger a webhook when internal event is emitted
+
+## Installation
+
+   ```bash
+   npm install edrm-webhook
+   ```
+
+## Environment Variables
+None
+
+
+## Routes
+- /webhook (Default Rest CRUD) : Manage webhooks
+
--- webhook/routes/webhook.router.js
+++ webhook/routes/webhook.router.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const Webhook = require('./models/webhook'); // Assure-toi que le chemin est correct
 const RouterBase = require('./router'); // Assure-toi que le chemin est correct
+const auth = require('endurance-core/lib/auth');
 
-const router = Router();
-RouterBase.autoWire(router, Webhook, 'Webhook');
+const router = RouterBase();
+
+const restrictWebhookAccess = {
+  checkPermissions: auth.checkUserPermissions(['canManageWebhooks']),
+  restrictToOwner: auth.restrictToOwner((req) => req.webhook.userId)
+};
+
+// Utilisation de l'autowire avec les restrictions d'accès
+RouterBase.autoWire(router, Webhook, 'Webhook', restrictWebhookAccess);
 
 module.exports = router;
